e72fa944de4b700811cf930c7b8f19d594e00440
Анна Гловяк <anna.kmeb@gmail.com>
2024-06-28 15:13:35 +0300

    Fixed supabase nuxt module version, added user card, logout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
-import { defineNuxtConfig } from "nuxt";
+// import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "@nuxtjs/supabase"],
+  supabase: {
+    redirect: false,
+  },
 });
