377461e223855ec3abbf652269cb104308ffc7a3
Анна Гловяк <anna.kmeb@gmail.com>
2024-07-17 20:14:52 +0300

    prerendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  nitro: {
+    prerender: {
+      routes: ["/landing"],
+    },
+  },
   modules: [
     "@nuxtjs/tailwindcss",
     "@vueuse/nuxt",
