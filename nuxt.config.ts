df27ac3dafc80842f4a446ee0142b0e1d14906f4
Анна Гловяк <anna.kmeb@gmail.com>
2024-07-17 20:33:02 +0300

    use images from assets, reduce img size by vite-plugin-vsharp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,13 @@
-// import { defineNuxtConfig } from "nuxt";
+import vsharp from "vite-plugin-vsharp";
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   nitro: {
     prerender: {
       routes: ["/landing"],
     },
+  },
+  vite: {
+    plugins: [vsharp()],
   },
   modules: [
     "@nuxtjs/tailwindcss",
