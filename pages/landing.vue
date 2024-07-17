1c5df6e8cfe7a77d4aeeb72b9c1d243650389869
Анна Гловяк <anna.kmeb@gmail.com>
2024-07-17 21:13:34 +0300

    lazy loading components

--- pages/landing.vue
+++ pages/landing.vue
@@ -59,7 +59,7 @@
       </li>
     </ul>
   </Section>
-  <Payment v-if="showPayment" @close="showPayment = false" />
+  <LazyPayment v-if="showPayment" @close="showPayment = false" />
 </template>
 
 <script setup lang="ts">
